6aacf235eefa6a5faa33c3b6182fddc27e07fea9
AbduGad <abdo79gad@gmail.com>
2024-05-30 00:04:04 +0300

    tasks completed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((responses) => (
+      responses.map((response) => ({
+        status: response.status,
+        value: response.status === 'fulfilled' ? response.value : String(response.reason),
+      }))
+    ));
+}
