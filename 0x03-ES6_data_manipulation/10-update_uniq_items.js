c604766755c5faffee5b2093957ef16c4af3939b
AbduGad <abdo79gad@gmail.com>
2024-06-04 19:46:54 +0300

    tasks 5 - 11

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  for (const elements of map) {
+    if (elements[1] === 1) {
+      map.set(elements[0], 100);
+    }
+  }
+  return map;
+}
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!startString || !startString.length) {
+    return res;
+  }
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) {
+      res += `${element.slice(startString.length)}-`;
+    }
+  });
+  return res.slice(0, res.length - 1);
+}
