dba961e115e39099ba1709243a8f5449492b3b42
AbduGad <abdo79gad@gmail.com>
2024-07-25 21:18:20 +0300

    "Added calculateNumber function and unit tests in 0-calcul.js and 0-calcul.test.js, respectively, and updated package.json with dependencies and scripts."

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+function calculateNumber(a, b) {
+  const an = Math.round(a);
+  const bn = Math.round(b);
+  const c = an + bn;
+  return c;
+}
+
+module.exports = calculateNumber;
